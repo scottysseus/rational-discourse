e89f3f1b7d0c189be4a88255d2698ca45cd7b4e9
scottysseus <sweidenkopf@gmail.com>
2021-01-27 18:13:34 -0500

    added tungstenite dependency

--- server-src/rational-discourse-server/src/main.rs
+++ server-src/rational-discourse-server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::server::accept;
+
+/// A WebSocket echo server
+fn main () {
+    let server = TcpListener::bind("127.0.0.1:3001").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+                println!(msg.);
+            }
+        });
+    }
 }
